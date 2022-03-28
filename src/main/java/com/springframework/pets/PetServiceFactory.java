41182a7e7012b42ccb9e4debc8dba2fa0fd69fb5
Rob Glasener <robglasener@me.com>
2022-03-28 15:11:20 -0700

    xml configuration

--- src/main/java/com/springframework/pets/PetServiceFactory.java
+++ src/main/java/com/springframework/pets/PetServiceFactory.java
@@ -3,7 +3,7 @@ package com.springframework.pets;
 /**
  * Created by jt on 2/20/21.
  */
-public class PetServiceFactory {
+public class  PetServiceFactory {
 
     public PetService getPetService(String petType){
         switch (petType){
