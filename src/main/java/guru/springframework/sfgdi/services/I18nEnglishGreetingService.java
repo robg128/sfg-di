4eafd22de4af08f6af9b76780dda1fe7eb37ddd1
Rob Glasener <robglasener@me.com>
2022-03-28 14:44:55 -0700

    DI in Java Configuration

--- src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -1,10 +1,18 @@
 package guru.springframework.sfgdi.services;
 
 
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
+
 /**
  * Created by jt on 12/27/19.
  */
 public class I18nEnglishGreetingService implements GreetingService {
+    private final EnglishGreetingRepository greetingRepository;
+
+    public I18nEnglishGreetingService(EnglishGreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+
     @Override
     public String sayGreeting() {
         return "Hello World - EN";
